b502dc5b2f9ff80cc4a8ef779dc8a1d9217ed285
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-07-08 10:30:33 -0700

    setting up some transaction notes

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -8,6 +8,12 @@ import (
 	"testing"
 )
 
+/*
+ATOMICITY: All operations complete successfully or transaction fails and db is unchanged
+Consistency: db state is valid after transaction. All constraints satisfied.
+Isolation: Concurrent transactions do not affect each other.
+Durability: Data written by a successful transaction must be recorded in persistent storage.
+*/
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
