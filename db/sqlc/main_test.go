605c3e7409614ce14c08e822a6f64887612f1be9
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-07-07 19:38:11 -0700

    Account Create Test

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("Can't connect to DB!")
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
