1e4593bd3fae918038084b9ab0eb5cf61068e051
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-07-08 07:36:43 -0700

    adding rando methods to tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,14 +3,20 @@ package db
 import (
 	"context"
 	"github.com/stretchr/testify/require"
+	"goBank/util"
 	"testing"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "tom",
-		Balance:  100,
-		Currency: "USD",
+		//Owner:    "tom",
+		//Balance:  100,
+		//Currency: "USD",
+
+		//Randomized instead
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
