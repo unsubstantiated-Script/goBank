eb1b2618e2d15b92fc51e40a5ea2dc1c7b3aef4f
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-07-22 10:02:35 -0700

    Setting up HTTP Gin

--- api/account.go
+++ api/account.go
@@ -1,1 +1,40 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "goBank/db/sqlc"
+	"net/http"
+)
+
+type createAccountRequest struct {
+	// binding: required added to validate the json via Gin
+	Owner string `json:"owner" binding:"required"`
+	// Removing this param as it needs to default to 0 when setup
+	//Balance  int64  `json:"balance"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+
+	//Handling bad data responses
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorReponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
