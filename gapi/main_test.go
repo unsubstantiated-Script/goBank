a9277552bd0ca2b12769c18982c7af5ad19c1e60
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-11-18 09:55:23 -0800

    Create User Tests Done

---
+++ gapi/main_test.go
@@ -0,0 +1,22 @@
+package gapi
+
+import (
+	"github.com/stretchr/testify/require"
+	db "goBank/db/sqlc"
+	"goBank/util"
+	"goBank/worker"
+	"testing"
+	"time"
+)
+
+func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store, taskDistributor)
+	require.NoError(t, err)
+
+	return server
+}
